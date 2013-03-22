9c9fbb12b5a7ee44f6903944b0c233a6455d2cbc
Gabriel Hopper <gabe@ustyme.com>
2013-03-21 18:13:59 -0700

    map mongo _id to backbone id.

---
+++ public/js/app-mongo-model.js
@@ -0,0 +1,19 @@
+(function (undefined) {
+    // Common JS // require JS
+    var _, $, Backbone, exports;
+    if (typeof window === 'undefined' || typeof require === 'function') {
+        $ = require('jquery');
+        _ = require('underscore');
+        Backbone = require('backbone');
+        exports = Backbone;
+        if (module) module.exports = exports;
+    } else {
+        $ = this.$;
+        _ = this._;
+        Backbone = this.Backbone;
+        exports = this;
+    }
+
+    Backbone.Model.prototype.idAttribute = "_id";
+
+})();

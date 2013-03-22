f245b9215a3ab8179d2b73b60e122d9bd75a341c
Gabriel Hopper <gabe@ustyme.com>
2013-03-22 09:38:38 -0700

    add publish to app-mongo-model.

--- public/js/app-mongo-model.js
+++ public/js/app-mongo-model.js
@@ -16,4 +16,30 @@
 
     Backbone.Model.prototype.idAttribute = "_id";
 
+    //TODO: move to it's own js
+    Backbone.Model.prototype.register = function(logic) {
+        this.logic = logic;
+    };
+
+    Backbone.Model.prototype.publish = function(socket, method, model, cb) {
+        if(this.key) {
+            socket.emit('publish', {
+                method: method,
+                logic: this.key
+            }, model, cb);
+        }
+    };
+    Backbone.Collection.prototype.register = function(key) {
+        this.key = key;
+    };
+
+    Backbone.Collection.prototype.publish = function(socket, method, collection, cb) {
+        if(this.key) {
+            socket.emit('publish', {
+                method: method,
+                logic: this.key
+            }, collection, cb);
+        }
+    };
+
 })();

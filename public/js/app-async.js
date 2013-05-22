f542598a82e2015dcdd24413acab9c8887befcb6
Gabriel Hopper <gabe@ustyme.com>
2013-05-22 15:25:32 -0700

    simple async.parallel

---
+++ public/js/app-async.js
@@ -0,0 +1,21 @@
+/*global PhoneGap:false, require:false */
+(function ($, _, app) {
+    "use strict";
+    app.async = app.async || {};
+
+    app.async.parallel = app.async.parallel ||  function (call, cb) {
+        var total = call.length;
+        _.forEach(call, function(fn){
+            fn(function(err, result){
+                if(err) {
+                    cb(err);
+                }
+                total--;
+                if(total==0){
+                    cb();
+                }
+            })
+        });
+    };
+
+})($, _, window["jolira-app"]);
